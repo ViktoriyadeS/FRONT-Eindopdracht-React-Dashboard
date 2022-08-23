fa2283d4241b2ef496d9a72872a14ffe221d2ae3
Viktoriya <81090951+ViktoriyadeS@users.noreply.github.com>
2022-08-23 12:24:49 +0200

    Update BarChart.js

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -92,6 +92,7 @@ const BarChart = (props) => {
         },
       },
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <Bar options={chartOptions} data={chartData} />;
